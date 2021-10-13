c64fa43b172064aa64ebb00281d81e75af052f79
Grant Orchard <grant.a.orchard@gmail.com>
2021-10-13 18:02:14 +1100

    Initial commit for provider configuration

---
+++ 091604009711.tf
@@ -0,0 +1,14 @@
+provider "aws" {
+    alias = 091604009711
+		access_key = data.vault_aws_access_credentials.091604009711.access_key
+	  secret_key = data.vault_aws_access_credentials.091604009711.secret_key
+		token = data.vault_aws_access_credentials.091604009711.security_token
+}
+
+data "vault_aws_access_credentials" "091604009711" {
+	backend = "aws"
+	type = "sts"
+	role = "account_management"
+	role_arn = "arn:aws:iam::091604009711:role/OrganizationAccountAccessRole"
+	ttl = "30m"
+}
