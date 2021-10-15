450288ece8ce2f7c4722a624738df37051113c10
Grant Orchard <go@hashicorp.com>
2021-10-15 18:31:18 +1100

    add provider reconfiguration

--- main.tf
+++ main.tf
@@ -3,11 +3,11 @@ locals {
 }
 
 module "vpc" {
-	for_each = toset([ for account in local.aws_accounts: account.id ])
+	for_each = toset(formatlist("aws_", [ for account in local.aws_accounts: account.id ]))
   source  = "terraform-aws-modules/vpc/aws"
   version = "3.8.0"
 	providers = {
-    aws = "${aws_}each.value"
+    aws = each.value
 	}
   # insert the 21 required variables here
 }
