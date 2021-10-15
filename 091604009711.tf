1c881131511a0778d7b53ef79322113aebd2011b
Grant Orchard <grant.a.orchard@gmail.com>
2021-10-15 17:18:43 +1100

    Initial commit for provider configuration

--- 091604009711.tf
+++ 091604009711.tf
@@ -1,11 +1,11 @@
 provider "aws" {
-    alias = "091604009711"
+    alias = "aws_091604009711"
 		access_key = data.vault_aws_access_credentials.091604009711.access_key
 	  secret_key = data.vault_aws_access_credentials.091604009711.secret_key
 		token = data.vault_aws_access_credentials.091604009711.security_token
 }
 
-data "vault_aws_access_credentials" "091604009711" {
+data "vault_aws_access_credentials" "aws_091604009711" {
 	backend = "aws"
 	type = "sts"
 	role = "terraform"
