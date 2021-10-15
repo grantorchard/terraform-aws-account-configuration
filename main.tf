d99bd1a8e2dc826b7eb37609cd745dcdf3f8884b
Grant Orchard <go@hashicorp.com>
2021-10-15 18:07:40 +1100

    add provider reconfiguration

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 locals {
-	aws_accounts = data.terraform_remote_state.this.account_details
+	aws_accounts = data.terraform_remote_state.this.outputs.account_details
 }
 
 module "vpc" {
-	for_each = { for account in aws_accounts: account => account.id }
+	for_each = { for account in local.aws_accounts: account => account.id }
   source  = "terraform-aws-modules/vpc/aws"
   version = "3.8.0"
   # insert the 21 required variables here
