4b073a6584cd22f21ee85df9d83324d4613350c6
Grant Orchard <go@hashicorp.com>
2022-09-19 17:08:23 +1000

    bump provider to v4

--- providers.tf
+++ providers.tf
@@ -2,8 +2,7 @@ terraform {
   required_providers {
     aws = {
       source = "hashicorp/aws"
-      version = "~>3"
-			#configuration_aliases = [ aws.aws_091604009711, aws.aws_262534729993 ]
+      version = "~> 4.31"
     }
   }
 }
